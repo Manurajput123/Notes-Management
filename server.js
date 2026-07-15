3ccdbada3920abc5df7667e88e97dc70b3911e76
Manurajput123 <chauhanhimanshu0203@gmail.com>
2026-07-15 17:21:18 +0000

    security purpose

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.use(express.static(path.join(__dirname, "public")));
 /* ===========================
    MongoDB Connection
 =========================== */
+
 require("dotenv").config();
 
 mongoose.connect(process.env.MONGO_URI)
